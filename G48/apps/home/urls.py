47d10cef891eb27a59a8306e5061d9cbbad04bb1
PanXiong <1357211280@qq.com>
2018-08-06 14:13:52 +0800

    整理代码，删除不用的代码

--- G48/apps/home/urls.py
+++ G48/apps/home/urls.py
@@ -7,7 +7,6 @@ app_name = 'home'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^test/', views.test, name='test'),
     url(r'^search_result/', views.search_result, name='search_result'),
     url(r'^config/$', views.config, name='config'),
 ]
--- G48/apps/home/websocket.py
+++ G48/apps/home/websocket.py
@@ -5,7 +5,7 @@ import hashlib
 import socket
 import base64
 import views
-from views import fuzzy_query, fuzzy_query_test
+from views import fuzzy_query_test
 
 global global_data1
 global_data1 = {}
@@ -22,13 +22,8 @@ class websocket_thread(threading.Thread):
             data = self.connection.recv(1024)
             if data is not None:
                 real_data = parse_data(data)
-                # print type(re)
-                # print re
                 import json
                 query_info_dict = json.loads(real_data)
-                print query_info_dict
-                # fuzzy_query(query_info_dict['keyword'], global_data1, self.connection, files_counts)
-                # query_info_dict['keyword'],  query_info_dict['tableType'],
                 fuzzy_query_test(global_data1, self.connection, query_info_dict)
 
 
@@ -73,7 +68,6 @@ if __name__ == '__main__':
     # filepath = ur'F:\Project\H37\H37_xls_search\05Data'
     filepath = ur'F:\Project\H37\H37_xls_search\test_file'
 
-    # files_counts, global_data1 = views.datas_form_files(filepath)
     global_data1 = views.datas_form_files_test(filepath)
 
     while True:
